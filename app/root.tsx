4a9b1b5216373f4b9859e31438e1787be3e29223
Christian Rotzoll <ch.rotzoll@gmail.com>
2024-12-14 10:16:11 +0100

    chore: remove unneeded google fonts preconnect

--- app/root.tsx
+++ app/root.tsx
@@ -1,21 +1,7 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
-import type { LinksFunction } from "react-router";
 import { Theme } from "@radix-ui/themes";
 
 import "./tailwind.css";
-
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
