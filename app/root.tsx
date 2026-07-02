6f7e7ce8b93e9941c537ba70b8813d7638555408
Christian Rotzoll <ch.rotzoll@gmail.com>
2026-07-02 21:42:17 +0200

    fix(a11y): add main landmark to app layout

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<Links />
 			</head>
 			<body>
-				{children}
+				<main>{children}</main>
 				<ScrollRestoration />
 				<Scripts />
 			</body>
